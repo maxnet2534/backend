45f8a8a4dc007aa539c59f4c2b0b7e8e56518a71
maxnet2534 <150881606+maxnet2534@users.noreply.github.com>
2023-11-25 13:21:16 +0700

    update 11/25/2023 01:21

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,5 +1,5 @@
 const User = require('../models/User');
-const Note = require('../models/Note');
+//const Note = require('../models/Note');
 const asyncHandler = require('express-async-handler');
 const bcrypt = require('bcrypt');
 
@@ -18,10 +18,10 @@ const getAllUsers = asyncHandler(async (req, res) => {
 // @route POST /users
 // @access Private
 const createNewUser = asyncHandler(async (req, res) => {
-    const { username, password, roles } = req.body;
+    const { username, password, prefix, firstName, lastName, studentId, degree, faculty, department, mail, tel, roles } = req.body;
 
     // Confirm data
-    if (!username || !password || !Array.isArray(roles) || !roles.length) {
+    if (!username || !password || !prefix || !firstName || !lastName || !studentId || !degree || !faculty || !department || !mail || !tel || !Array.isArray(roles) || !roles.length) {
         return res.status(400).json({ message: 'All fields are required'});
     }
 
@@ -30,11 +30,16 @@ const createNewUser = asyncHandler(async (req, res) => {
     if (duplicate) {
         return res.status(409).json({ message: 'Duplicate username'});
     }
+    
+    const duplicateMail = await User.findOne({ mail }).lean().exec();
+    if (duplicateMail) {
+        return res.status(409).json({ message: 'Duplicate email address'});
+    }
 
     // Hash password
     const hashedPwd = await bcrypt.hash(password, 10);
 
-    const userObject = { username, "password": hashedPwd, roles };
+    const userObject = { username, "password": hashedPwd, prefix, firstName, lastName, studentId, degree, faculty, department, mail, tel, roles };
 
     // Create and store new user
     const user = await User.create(userObject);
@@ -50,10 +55,10 @@ const createNewUser = asyncHandler(async (req, res) => {
 // @route PATCH /users
 // @access Private
 const updateUser = asyncHandler(async (req, res) => {
-    const { id, username, roles, active, password } = req.body;
+    const { id, username, password, prefix, firstName, lastName, studentId, degree, faculty, department, mail, tel, roles } = req.body;
 
     // Confirm data
-    if (!id || !username || !Array.isArray(roles) || !roles.length || typeof active !== 'boolean' ) {
+    if (!id || !username || !prefix || !firstName || !lastName || !studentId || !degree || !faculty || !department || !mail || !tel || !Array.isArray(roles) || !roles.length ) {
         return res.status(400).json({ message: 'All fields are required.'});
     }
 
@@ -70,15 +75,27 @@ const updateUser = asyncHandler(async (req, res) => {
         return res.status(409).json({ message: 'Duplicate username.'});
     }
 
+    // Chang variable
     user.username = username;
+    user.prefix = prefix;
+    user.firstName = firstName;
+    user.lastName = lastName;
+    user.studentId = studentId;
+    user.degree = degree;
+    user.faculty = faculty;
+    user.department = department;
+    user.mail = mail;
+    user.tel = tel;
     user.roles = roles;
-    user.active = active;
+    user.roles = roles;
 
+    // if have password
     if (password) {
         // Hash password
         user.password = await bcrypt.hash(password, 10) // set rounds
     }
 
+    // Update User to mongoDB
     const updatedUser = await user.save();
 
     res.json({ message: `${updatedUser.username} updated.`});
@@ -94,22 +111,24 @@ const deleteUser = asyncHandler(async (req, res) => {
         return res.status(400).json({ message: 'User ID required'});
     }
 
-    const note = await Note.findOne({ user: id}).lean().exec();
+    // check have a document before delete
+/*     const note = await Note.findOne({ user: id}).lean().exec();
     if (note) {
         return res.status(400).json({ message: 'User has assigned notes'});
-    }
+    } */
 
+    // Check have a id
     const user = await User.findById(id).exec();
 
     if (!user) {
         return res.status(400).json({ message: 'User not found.'});
     }
 
-    const result = await user.deleteOne();
-
-    const reply = `Username ${result.username} with ID ${result._id} deleted`
-
+    const reply = `Username ${user.username} with ID ${user._id} deleted`
     res.json(reply);
+    
+    await user.deleteOne();
+
 })
 
 module.exports = {
--- models/User.js
+++ models/User.js
@@ -9,6 +9,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    prefix: {
+        type: String,
+        required: true
+    },
     firstName: {
         type: String,
         required: true
